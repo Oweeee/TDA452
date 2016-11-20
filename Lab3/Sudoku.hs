7326534ebd92a1d3942a68ad9c945808adee3b13
Oskar Willman <oskar_willman@live.se>
2016-11-20 22:30:48 +0100

    began working on lab 3. added first function.

--- Lab3/Sudoku.hs
+++ Lab3/Sudoku.hs
@@ -1,1 +1,13 @@
 --Sudoku.hs
+
+--A program to solve sudoku-puzzles.
+
+--datatype sudoku
+data Sudoku = Sudoku { rows :: [[Maybe Int]] }
+
+--function to create a sudoku-board with only blank cells.
+allBlankSudoku :: Sudoku
+Sudoku rows = [ [ Nothing | x <- [1..9] ] | x <- [1..9] ]
+
+--function that makes sure the given type is a sudoku.
+isSudoku :: Sudoku -> Bool
