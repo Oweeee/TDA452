eb2865c5a742b57d6767d03517aa10f70440468d
Oskar Willman <oskar_willman@live.se>
2016-12-13 21:10:05 +0100

    Functioning parser for integer expressions

--- Lab 4/Expr.hs
+++ Lab 4/Expr.hs
@@ -1,19 +1,22 @@
 import Parsing
+import Data.Maybe
+import Data.Char
 
 data Expr = Num Double
-          | Var   
+          | Var Char
           | Add Expr Expr
           | Mul Expr Expr
           | Sin Expr
           | Cos Expr
+          deriving (Show)
 
 showExpr :: Expr -> String
 showExpr (Num n)     = show n
-showExpr Var         = "x"
-showExpr (Add e1 e2) = (showExpr e1) ++ " + " ++ (showExpr e2)
-showExpr (Mul e1 e2) = (showFactor e1) ++ " * " ++ (showFactor e2)
-showExpr (Sin e)     = "sin (" ++ showExpr e ++ ")"
-showExpr (Cos e)     = "cos (" ++ showExpr e ++ ")"
+showExpr (Var c)     = "x"
+showExpr (Add e1 e2) = (showExpr e1) ++ "+" ++ (showExpr e2)
+showExpr (Mul e1 e2) = (showFactor e1) ++ "*" ++ (showFactor e2)
+showExpr (Sin e)     = "sin(" ++ showExpr e ++ ")"
+showExpr (Cos e)     = "cos(" ++ showExpr e ++ ")"
 
 
 showFactor :: Expr -> String
@@ -21,14 +24,39 @@ showFactor (Add e1 e2) = "(" ++ showExpr (Add e1 e2) ++ ")"
 showFactor e           = showExpr e
 
 eval :: Expr -> Double -> Double
-eval (Num n) _     = n
-eval Var n         = n
+eval (Num n) x     = n
+eval (Var c) x     = x
 eval (Add e1 e2) x = (eval e1 x) + (eval e2 x)
 eval (Mul e1 e2) x = (eval e1 x) * (eval e2 x)
 eval (Sin e) x     = sin (eval e x)
 eval (Cos e) x     = cos (eval e x)
 
 readExpr :: String -> Maybe Expr
-readExpr s = 
+readExpr s = let s' = filter (not.isSpace) s
+             in case parse expr s' of
+                Just (e,"") -> Just e
+                _           -> Nothing
+
+double :: Parser Double
+double = (oneOrMore digit >>= return . read) <|> 
+         fmap negate (char '-' *> double)
+
+num :: Parser Expr
+num = Num <$> double
+
+expr = foldr1 Add <$> chain term (char '+')
+
+term = foldr1 Mul <$> chain factor (char '*')
+
+sinFunc = Sin <$> ((char 's') *> (char 'i') *> (char 'n') *> factor) 
+
+cosFunc = Cos <$> ((char 'c') *> (char 'o') *> (char 's') *> factor) 
+
+var = Var <$> (char 'x')
+
+factor = char '(' *> expr <* char ')' <|> num <|> 
+         sinFunc <|> cosFunc <|> var
+
+
 
 
