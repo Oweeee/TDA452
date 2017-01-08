2d8efb2befdd89c9f37ff12968ef11763840a5f7
Hampus Rönström <h_ronstrom@hotmail.com>
2017-01-08 12:06:21 +0100

    changed expr to exprold

--- Lab 4/Expr.hs
+++ Lab 4/Expr.hs
@@ -57,39 +57,68 @@ factor = char '(' *> expr <* char ')' <|> num <|>
          sinFunc <|> cosFunc <|> var
 
 simplify :: Expr -> Expr
-simplify e | e == simplify' e = e
-simplify e = simplify' e
+simplify e = do 
+              let e' = simplify' e
+              let result = if e' == e then e' else simplify e'
+              result
+
+
 
 simplify' :: Expr -> Expr
 simplify' e = case e of 
-        (Num e')                 -> Num e'
-        (Add (Num 0) e')         -> simplify e'
-        (Add e' (Num 0))         -> simplify e'
-        (Mul (Num 0) _)          -> Num 0
-        (Mul _ (Num 0))          -> Num 0
-        (Mul (Num 1) e')         -> simplify e'
-        (Mul e' (Num 1))         -> simplify e'
-        (Add (Num n) (Num m))    -> Num (n+m)
-        (Add e1 e2)              -> simplify (Add (simplify e1) (simplify e2))
-        (Mul (Num n) (Num m))    -> Num (n*m)
-        (Mul e1 e2)              -> simplify (Mul (simplify e1) (simplify e2))
-        (Sin e')                 -> simplify (Sin (simplify e'))
-        (Cos e')                 -> simplify (Cos (simplify e'))    
-        otherwise                -> e
+
+        -- base cases
+        (Num e')                             -> Num e'
+        (Var 'x')                            -> Var 'x'
+
+        --adding 0
+        (Add (Num 0.0) e')                   -> simplify e'
+        (Add e' (Num 0.0))                   -> simplify e'
+
+        --multiplying by 0
+        (Mul (Num 0.0) e')                   -> Num 0
+        (Mul e' (Num 0.0))                   -> Num 0
+
+        --multiplying by 1
+        (Mul (Num 1.0) e')                   -> simplify e'
+        (Mul e' (Num 1.0))                   -> simplify e'
+
+        --simplifying additions
+        (Add (Num n) (Num m))                -> Num (n+m)
+        (Add e1 e2) | e1 == e2               -> (Mul (Num 2) (simplify e1))
+        (Add (Mul (Num n) e1) e2) | e1 == e2 -> (Mul (Num (n+1)) (simplify e1))
+        (Add e1 (Mul (Num n) e2)) | e1 == e2 -> (Mul (Num (n+1)) (simplify e1))
+        (Add e1 e2)                          -> (Add (simplify e1) (simplify e2))
+
+        --simplifying multiplications
+        (Mul (Num n) (Num m))                -> Num (n*m)
+        (Mul (Var 'x') (Var 'x'))            -> (Mul (Var 'x') (Var 'x'))
+        (Mul e' (Num n))                     -> (Mul (Num n) (simplify e'))
+        (Mul (Num n) (Var 'x'))              -> (Mul (Num n) (Var 'x'))
+        (Mul e' (Var 'x'))                   -> (Mul (Var 'x') (simplify e'))
+        (Mul (Num n) (Mul e' (Num m)))       -> (Mul (Num (n*m)) (simplify e'))
+        (Mul (Num n) (Mul (Num m) e'))       -> (Mul (Num (n*m)) (simplify e'))
+        (Mul (Var 'x') (Mul e' (Num n)))     -> (Mul (Num n) (Mul (simplify e') (Var 'x')))
+        (Mul (Var 'x') (Mul (Num n) e'))     -> (Mul (Num n) (Mul (simplify e') (Var 'x')))
+        (Mul e1 e2)                          -> (Mul (simplify e1) (simplify e2))
+
+        --simplifying functions
+        (Sin e')                             -> (Sin (simplify e'))
+        (Cos e')                             -> (Cos (simplify e'))    
 
 
 differentiate :: Expr -> Expr
 differentiate e = simplify (differentiate' e)
-  where
-    differentiate' :: Expr -> Expr
-    differentiate' e = case e of
-      (Num n)           -> Num 0
-      (Var x)           -> Num 1
+  
+differentiate' :: Expr -> Expr
+differentiate' e = case e of
+      (Num n)           -> Num 0.0
+      (Var x)           -> Num 1.0
       (Add e1 e2)       -> (Add (differentiate' e1) (differentiate' e2))
       (Mul e1 e2)       -> (Add (Mul e1 (differentiate' e2)) 
                               (Mul (differentiate' e1) e2))
       (Sin e)           -> (Cos e)
-      (Cos e)           -> (Mul (Num (-1)) (Sin e))
+      (Cos e)           -> (Mul (Num (-1.0)) (Sin e))
 
 
 
